80ffc353f73bb3dd4e3c795307c913c8a5bf424d
sabokdas14 <sabokdas14@gmail.com>
2026-05-25 23:24:10 +0600

    Problem-1 solved

--- solutions.ts
+++ solutions.ts
@@ -1,2 +1,7 @@
-const course:string = "Next level web development";
-console.log(course);
+//Problem-----1
+
+const filterEvenNumber =(input:number[]):number[]=>{
+    return input.filter(num => num%2===0);
+};
+
+console.log(filterEvenNumber([1,2,3,4,5,6]));
