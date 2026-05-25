877c4a13295f13eb76f372854b6a089b414792ea
sabokdas14 <sabokdas14@gmail.com>
2026-05-25 23:36:40 +0600

    Problem-2 solved

--- solutions.ts
+++ solutions.ts
@@ -4,4 +4,15 @@ const filterEvenNumber =(input:number[]):number[]=>{
     return input.filter(num => num%2===0);
 };
 
-console.log(filterEvenNumber([1,2,3,4,5,6]));
+// console.log(filterEvenNumber([1,2,3,4,5,6]));
+
+
+//Problem-----2
+
+const reverseString = (something:string)=>{
+    return something.split("").reverse().join("");
+}
+
+
+// const some:string = "typescript";
+// console.log(reverseString(some));
