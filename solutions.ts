e1412bfca70ee03332b90d9950e74870fb02f11e
sabokdas14 <sabokdas14@gmail.com>
2026-05-26 10:59:15 +0600

    Problem-3 solved

--- solutions.ts
+++ solutions.ts
@@ -13,6 +13,22 @@ const reverseString = (something:string)=>{
     return something.split("").reverse().join("");
 }
 
-
 // const some:string = "typescript";
 // console.log(reverseString(some));
+
+//Problem-----3
+
+type StringOrNumber = string|number;
+
+const checkType =(input:StringOrNumber)=>{
+    if(typeof input === "string"){
+        return "String";
+    }
+    else{
+        return "Number";
+    }
+};
+
+console.log(checkType("0"));
+console.log(checkType(42));
+
