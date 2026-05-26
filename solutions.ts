723d8156815d9bf5d5a1d5e786d8d91d8bd4f15c
sabokdas14 <sabokdas14@gmail.com>
2026-05-26 12:18:37 +0600

    Problem-7 solved

--- solutions.ts
+++ solutions.ts
@@ -109,3 +109,15 @@ const student = new Student("Alice",20,"A");
 
 // console.log(student.getDetails());
 
+
+//Problem-----7
+
+function getIntersection<T>(array1:T[],array2:T[]):T[]{
+    return array1.filter(element => array2.includes(element));
+};
+
+const arr1=[1,2,3,4,5];
+const arr2=[3,4,5,6,7];
+
+console.log(getIntersection(arr1,arr2));
+
