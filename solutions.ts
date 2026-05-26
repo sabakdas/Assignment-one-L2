ce7fb914f12a016c83e53b2305472bb4feb792d2
sabokdas14 <sabokdas14@gmail.com>
2026-05-26 11:56:08 +0600

    Problem-6 solved

--- solutions.ts
+++ solutions.ts
@@ -75,3 +75,37 @@ const book={
 
 // console.log(toggleReadStatus(book));
 
+//Problem-----6
+
+class Person {
+    name:string;
+    age:number;
+
+    constructor(name:string,age:number){
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
+
+class Student extends Person{
+    
+    grade:string;
+
+    constructor(name:string,age:number,grade:string){
+        super(name,age); 
+        this.grade=grade;
+
+    }
+
+    getDetails(){
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`
+        
+    }
+};
+
+const student = new Student("Alice",20,"A");
+
+// console.log(student.getDetails());
+
